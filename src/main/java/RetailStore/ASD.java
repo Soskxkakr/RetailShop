6aa0ac1022c6dd460a8164e162f367df12235c15
Reinaldo Taslim <Reinaldo Taslim@LAPTOP-8OK420B8>
2020-04-07 16:01:57 +0800

    CopyOnWriteArrayList

--- src/main/java/RetailStore/ASD.java
+++ src/main/java/RetailStore/ASD.java
@@ -2,34 +2,34 @@ package RetailStore;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.ArrayList;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  *
  * @author Reinaldo Taslim
  */
 public class ASD {
-    public static void main(String[] args) {
-        ArrayList<Map<String, String>> AL = new ArrayList<>();
+    public static void main(String[] args) {          
+        CopyOnWriteArrayList<Map<String, String>> AL = new CopyOnWriteArrayList<>();
         Map<String, String> MPDetails = new HashMap<>();
         MPDetails.put("Name", "Reinaldo");
         MPDetails.put("Age", "12");
         AL.add(MPDetails);
-        
+            
         Map<String, String> MPDetails2 = new HashMap<>();
         MPDetails2.put("Name", "Steven");
         MPDetails2.put("Age", "21");
         
         
-        Map<String, ArrayList<Map<String,String>>> MP = new HashMap<>();
+        Map<String, CopyOnWriteArrayList<Map<String,String>>> MP = new HashMap<>();
         MP.put("C1", AL);
         
         
-        for (ArrayList<Map<String, String>> X : MP.values()){
+        for (CopyOnWriteArrayList<Map<String, String>> X : MP.values()){
             X.add(MPDetails2);
             for (Map<String, String> Y : X){
-                if ( Y.get("Name").equals("Reinaldo") ){
-                    Y.put("Age", "20");
+                if ( Y.get("Name").equals("Steven") ){
+                    X.remove(Y);
                 }
             }
             System.out.println(X);
