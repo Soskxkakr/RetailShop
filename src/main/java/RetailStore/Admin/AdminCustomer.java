ee10a75582f06247f25caa212ca407d57c4be849
Reinaldo Taslim <Reinaldo Taslim@LAPTOP-8OK420B8>
2020-04-08 17:35:03 +0800

    AdminCustomer and Config

--- src/main/java/RetailStore/Admin/AdminCustomer.java
+++ src/main/java/RetailStore/Admin/AdminCustomer.java
@@ -1,14 +1,42 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package RetailStore.Admin;
 
+import RetailStore.ReadFile;
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import org.json.JSONObject;
 /**
  *
  * @author Reinaldo Taslim
  */
 public class AdminCustomer {
-    
+    private ReadFile RF = new ReadFile();
+    private Gson JSONConverter = new Gson();
+    public void DeleteCustomer(String AdminInput){
+        try{
+            JSONObject CustomerDataJson = RF.getJSONObject("C:\\Users\\Reinaldo Taslim\\Documents\\NetBeansProjects\\RetailStore\\target\\classes\\JSON\\Customer.json");
+            System.out.println(CustomerDataJson);
+            Map<String, ArrayList<Map<String, String>>> CustomerDataMap = JSONConverter.fromJson(CustomerDataJson.toString(), HashMap.class); // Convert JSON to HashMap
+            for (ArrayList<Map<String,String>> CustomerDetails : CustomerDataMap.values()){
+                for (Iterator<Map<String,String>> Iter = CustomerDetails.iterator(); Iter.hasNext(); ){
+                    Map<String,String> CDetails = Iter.next();
+                    if (CDetails.get("ID").equals(AdminInput)){
+                        Iter.remove();
+                    }
+                }
+            }           
+//            System.out.println(CustomerDataMap);
+            FileWriter FW = new FileWriter("C:\\Users\\Reinaldo Taslim\\Documents\\NetBeansProjects\\RetailStore\\target\\classes\\JSON\\Customer.json");
+            String NewMap = JSONConverter.toJson(CustomerDataMap);
+            FW.write(NewMap.toString());
+            FW.close();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        
+    }
 }
--- src/main/java/RetailStore/Main.java
+++ src/main/java/RetailStore/Main.java
@@ -1,4 +1,6 @@
 package RetailStore;
+
+import RetailStore.Admin.AdminCustomer;
 /**
  *
  * @author Reinaldo Taslim
@@ -6,6 +8,7 @@ package RetailStore;
 public class Main {
     public static void main(String[] args) {
         try {
+//            AdminCustomer AC = new AdminCustomer();
             new LoginPage().setVisible(true);
             System.out.println(System.getProperty("java.class.path"));
         } catch (Exception e){
